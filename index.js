2b76578936dd2ea455103c755cac1e2b71f5e080
Rahul N Bangera <imrahuludp@gmail.com>
2024-11-03 18:56:48 +0530

    Fixed a minor bug in the index.js file Due to async keyword not used

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.use(e.urlencoded({ extended: true }));
   // req.session.email = "testing";
 
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
 
   console.log(req.session.email);
   console.log(req.session.name);
