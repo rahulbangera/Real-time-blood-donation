6c6af54cd7e17f7c11184fa8808204144d6611f2
Rahul N Bangera <imrahuludp@gmail.com>
2024-11-12 19:49:31 +0530

    check if the service workers working

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -29,18 +29,38 @@ async function requestNotificationPermission() {
   }
 }
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-} else {
-  console.warn('Service Workers are not supported in this browser.');
+
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/firebase-messaging-sw.js')
+//     .then((registration) => {
+//       console.log('Service Worker registered with scope:', registration.scope);
+//     })
+//     .catch((error) => {
+//       console.error('Service Worker registration failed:', error);
+//     });
+// } else {
+//   console.warn('Service Workers are not supported in this browser.');
+// }
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistration("/firebase-messaging-sw.js").then((registration) => {
+      if (registration) {
+          console.log("Service Worker already registered with scope:", registration.scope);
+      } else {
+          navigator.serviceWorker
+              .register("/firebase-messaging-sw.js")
+              .then((registration) => {
+                  console.log("Service Worker registered with scope:", registration.scope);
+              })
+              .catch((error) => {
+                  console.error("Service Worker registration failed:", error);
+              });
+      }
+  });
 }
+
 
 
 requestNotificationPermission();
