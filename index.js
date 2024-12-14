49ea922344c39ca0c320dd4eb255ebd6368d725b
Rahul N Bangera <85377350+Bnir@users.noreply.github.com>
2024-12-14 07:55:52 +0530

    Fixed the bug of reset password

--- index.js
+++ index.js
@@ -1149,7 +1149,7 @@ app.post("/forgot-password", async (req, res) => {
     newToken.save();
 
     // Generate the password reset link
-    const resetLink = `${url}/${uniqueResetToken}`;
+    const resetLink = `${url}/reset-password/${uniqueResetToken}`;
 
     await transporter.sendMail({
       from: "coderangersverify@gmail.com",
